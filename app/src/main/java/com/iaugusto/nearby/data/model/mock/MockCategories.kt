7331e89de57401959a2811d9b5e6da39b080fb71
Igor Augusto <igoraugustobz@gmail.com>
2025-06-21 15:28:56 -0300

    :recycle: refactor: adicionar mock de categorias para o componente NearbyCategoryFilterChipList

---
+++ app/src/main/java/com/iaugusto/nearby/data/model/mock/MockCategories.kt
@@ -0,0 +1,21 @@
+package com.iaugusto.nearby.data.model.mock
+
+import com.iaugusto.nearby.data.model.Category
+
+val mockCategories = listOf(
+    Category(
+        id = "1",
+        name = "Alimentação"
+    ),
+    Category(
+        id = "2",
+        name = "Cinema"
+    ), Category(
+        id = "3",
+        name = "Farmácia"
+    ),
+    Category(
+        id = "4",
+        name = "Supermercado"
+    )
+)
